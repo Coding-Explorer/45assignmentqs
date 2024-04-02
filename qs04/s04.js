11ca1222c66b23ff08ebf11e7ca89ead34b6c3f4
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:14:48 +0500

    Add files via upload

---
+++ qs04/s04.js
@@ -0,0 +1,6 @@
+// 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+var name1 = (" Muhammad Ali Jinnah");
+console.log("".concat(name1, ", \u201C There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women. .\u201D "));
+//example 2
+console.log(name1, " \u201CThere are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women. .\u201D ");
---
+++ qs04/s04.ts
@@ -0,0 +1,9 @@
+// 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+//01
+let name1: string= (" Muhammad Ali Jinnah")
+
+console.log(`${name1}, “ There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women. .” `)
+
+//02
+console.log(name1,` “There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women. .” `)
