683a2e3ef6d0aa86d797164cee9376d84cea5b02
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:15:25 +0500

    Add files via upload

---
+++ qs05/s05.js
@@ -0,0 +1,6 @@
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable
+// called famous_person. Then compose your message and store it in a new variable called message.
+// Print your message.
+var famous_person = "Muhammad Ali Jinnah";
+var message2 = "With faith, discipline and selfless devotion to duty,there is nothing worthwhile that you cannot achieve.";
+console.log("".concat(famous_person, " said. \"").concat(message2, "\""));
