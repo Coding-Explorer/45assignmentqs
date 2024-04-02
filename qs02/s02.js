6644f46f657e5498e46432028b1677e32aad29d6
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:12:46 +0500

    Add files via upload

---
+++ qs02/s02.js
@@ -0,0 +1,6 @@
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person.
+// Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+// message to Vegetable sellerman Asif
+var Sellerman = "Asif";
+console.log("Hello ".concat(Sellerman, ", \"I need some fresh vegetables, Please give me some fresh vegetables ?\" "));
---
+++ qs02/s02.ts
@@ -0,0 +1,8 @@
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person.
+// Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+
+// message to Vegetable sellerman Asif
+let Sellerman: string = "Asif";
+
+console.log(`Hello ${Sellerman}, "I need some fresh vegetables, Please give me some fresh vegetables ?" `);
