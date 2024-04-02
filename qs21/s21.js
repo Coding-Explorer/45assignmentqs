91c3cf67c6e7e49f9b1f54a44623a62391bce8d9
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:30:24 +0500

    Add files via upload

---
+++ qs21/s21.js
@@ -0,0 +1,23 @@
+// Q21. They think of something you could store in a TypeScript Object. Write a program that
+// creates Objects containing these items.
+//01
+var Product = {
+    Name: "Enervit Syrup", Price: 100, Qty: 500, Sale: 50, Bal: 450
+};
+console.log(Product);
+//02
+var Febrol = {
+    Name: "Febrol Syrup",
+    Price: 100,
+    Qty: 500,
+    Sale: 50,
+    Bal: 450
+};
+var Panadol = {
+    Name: "Panadol Syrup",
+    Price: 100,
+    Qty: 300,
+    Sale: 100,
+    Bal: 400,
+};
+console.log(Febrol, "\n", Panadol);
---
+++ qs21/s21.ts
@@ -0,0 +1,32 @@
+// Q21. They think of something you could store in a TypeScript Object. Write a program that
+// creates Objects containing these items.
+
+//01
+
+let Product:{
+    Name:string, Price:number, Qty:number ,Sale:number, Bal:number} = {
+        Name:"Enervit Syrup", Price:100, Qty:500 ,Sale:50, Bal:450}
+console.log(Product)
+
+
+//02
+
+const Febrol = {
+    Name : "Febrol Syrup",
+    Price : 100,
+    Qty : 500,
+    Sale : 50,
+    Bal : 450
+}
+const Panadol = {
+    Name : "Panadol Syrup",
+    Price : 100,
+    Qty : 300,
+    Sale : 100,
+    Bal : 400,
+}
+
+console.log(Febrol,"\n",Panadol)
+
+
+
