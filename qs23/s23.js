acf22f91a15ebfaec94d246d897437b8d0a3c0f7
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:31:29 +0500

    Add files via upload

---
+++ qs23/s23.js
@@ -0,0 +1,38 @@
+// Q23. Conditional Tests: Write a series of conditional tests. Print a statement describing
+// each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// Test 01 True
+var number1 = 44;
+console.log("Is number1, equal to 22+22? I predict True.");
+console.log(number1 == 44);
+// Test 01 False
+console.log("Is number1, not equal to 22+22? I predict False.");
+console.log(number1 !== 44);
+// // Test 02 True
+console.log("Is number1 greater then 40? I predict True.");
+console.log(number1 > 40);
+// // Test 02 false
+console.log("Is number1 is not greater then 40? I predict False.");
+console.log(number1 < 40);
+// // // Test 03 True
+console.log("Is number1 multiplied by 2 equal to 88? I predict True.");
+console.log(number1 * 2 == 88);
+// // // Test 03 False
+console.log("Is number1 multiplied by 2 equal to 66? I predict False.");
+console.log(number1 * 2 == 66);
+// // // // Test 04 True
+console.log("Is number1 divided by 2 equal to 22? I predict True.");
+console.log(number1 / 2 == 22);
+// // // // Test 04 false
+console.log("Is number1 divided by 2 is not equal to 22? I predict False.");
+console.log(number1 / 2 !== 22);
+// // // // // Test 05 True
+console.log("Is number1 subtracted by 2 equal to 42? I predict True.");
+console.log(number1 - 2 == 42);
+// // // // // Test 05 false
+console.log("Is number1 Added by 2 equal to 42? I predict False.");
+console.log(number1 + 2 == 42);
