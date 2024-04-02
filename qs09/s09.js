b1fbc19a8346ef8236fdcab2b3ebed4b9a4aaf9f
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:18:17 +0500

    Add files via upload

---
+++ qs09/s09.js
@@ -0,0 +1,9 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number. Print that message.
+var favnum = 16;
+console.log("My Favourite Number Is ".concat(favnum));
+//02
+var favno1 = 46;
+var msg = "My favorite number is ".concat(favno1, ".");
+// Print the message
+console.log(msg);
---
+++ qs09/s09.ts
@@ -0,0 +1,13 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number. Print that message.
+
+
+let favnum: number = 16
+console.log(`My Favourite Number Is ${favnum}`)
+
+//02
+let favno1: number = 46;
+let msg: string = `My favorite number is ${favno1}.`;
+
+// Print the message
+console.log(msg);
