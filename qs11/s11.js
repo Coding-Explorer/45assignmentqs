c6619292ce8638062cbe59b0beb6ae4cd11c98b9
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:19:31 +0500

    Add files via upload

---
+++ qs11/s11.js
@@ -0,0 +1,13 @@
+//11. Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+var fnames = ["Adnan Khan", "Asif Khan", "Babar Khan", "Ismail Khan"];
+console.log(fnames[0]);
+console.log(fnames[1]);
+console.log(fnames[2]);
+console.log(fnames[3]);
+//02
+var fNames1 = ["Tasleem", "Rehan", "Bilal", "Ayaz", "Danish"];
+// Print each person's name one at a time
+for (var i = 0; i < fNames1.length; i++) { //Using For loop
+    console.log(fNames1[i]);
+}
---
+++ qs11/s11.ts
@@ -0,0 +1,19 @@
+//11. Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+
+
+let fnames: string[] = ["Adnan Khan", "Asif Khan", "Babar Khan", "Ismail Khan"];
+console.log(fnames[0])
+console.log(fnames[1])
+console.log(fnames[2])
+console.log(fnames[3])
+
+//02
+
+let fNames1: string[] = ["Tasleem", "Rehan", "Bilal", "Ayaz", "Danish"];
+
+// Print each person's name one at a time
+for (let i = 0; i < fNames1.length; i++) { //Using For loop
+  console.log(fNames1[i]);
+}
+
