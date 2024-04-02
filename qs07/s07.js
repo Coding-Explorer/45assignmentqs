5d0dd3128ce99151c1443d8f209a0369be5f18e4
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-04-02 21:16:54 +0500

    Add files via upload

---
+++ qs07/s07.js
@@ -0,0 +1,18 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result
+// in the number 8. Be sure to enclose your operations in print statements to see the results. 
+console.log(5 + 3);
+console.log(18 - 10);
+console.log(2 * 4);
+console.log(16 / 2);
+// Addition
+var additionResult = 5 + 3;
+console.log("Addition:", additionResult);
+// Subtraction
+var subtractionResult = 10 - 2;
+console.log("Subtraction:", subtractionResult);
+// Multiplication
+var multiplicationResult = 4 * 2;
+console.log("Multiplication:", multiplicationResult);
+// Division
+var divisionResult = 16 / 2;
+console.log("Division:", divisionResult);
---
+++ qs07/s07.ts
@@ -0,0 +1,25 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result
+// in the number 8. Be sure to enclose your operations in print statements to see the results. 
+
+
+console.log(5 + 3)
+console.log(18 - 10)
+console.log(2 * 4)
+console.log(16 / 2)
+
+
+// Addition
+let additionResult: number = 5 + 3;
+console.log("Addition:", additionResult);
+
+// Subtraction
+let subtractionResult: number = 10 - 2;
+console.log("Subtraction:", subtractionResult);
+
+// Multiplication
+let multiplicationResult: number = 4 * 2;
+console.log("Multiplication:", multiplicationResult);
+
+// Division
+let divisionResult: number = 16 / 2;
+console.log("Division:", divisionResult);
